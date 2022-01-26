e6b753515d1d9e0b14665ca83fb5dac836c45c95
Moses <moseskassin>
2022-01-26 19:02:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+document.addEventListener("click",jump)
+
+
+function jump(){
+    if(character.classList !="animate"){
+        character.classList.add("animate");
+    }
+character.classList.add("animate");
+
+setTimeout(function(){
+character.classList.remove("animate")
+},500);
+    
+
+}
+
+const checkDead = setInterval(function(){
+const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+
+if(blockLeft<20 && blockLeft> 0 && characterTop>= 130 ){
+    block.style.animation = "none";
+    block.style.display = "none";
+
+    alert("u lose.");
+}
+
+},10);
